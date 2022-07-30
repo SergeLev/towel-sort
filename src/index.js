31fa66ee9e96901d4fe67525f1fd0d85708a9d1f
SergeLev <106580232+SergeLev@users.noreply.github.com>
2022-07-30 11:24:56 +0300

    feat: towel-sort works

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
-
-// You should implement your task here.
-
 module.exports = function towelSort (matrix) {
-  return [];
+  if (!matrix) {return []}
+  let result = [];
+  for (let i=0; i<matrix.length; i++){
+      let smallArr = matrix[i].sort((a, b) => (i%2 != 0) ?  b - a : a - b);
+   result.push(smallArr);
+   }  
+  return result.flat();
 }
